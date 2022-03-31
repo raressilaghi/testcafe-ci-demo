a5b6ee8219d392fb5a239540f2bc7255bbae4e4e
raressilaghi <58920082+raressilaghi@users.noreply.github.com>
2022-03-31 15:35:44 +0300

    Add files via upload

---
+++ tests/new-test-script.js
@@ -0,0 +1,16 @@
+import { Selector } from 'testcafe';
+
+fixture `New Fixture`
+    .page `http://10.115.8.126:3002/en/`;
+    
+test(`New Test`, async t => {
+    await t
+        .typeText('header .ui-autocomplete-input', 'cana')    
+        .pressKey('shift')
+        .pressKey('enter')
+        .click(Selector('main a').withText(' Quick view').nth(3))
+        .click(Selector('#add-to-cart-or-refresh button').withText(' ADD TO CART'))
+        .click(Selector('#blockcart-modal a').withText('PROCEED TO CHECKOUT'))
+        .pressKey('shift')
+        .click(Selector('main a').withText('PROCEED TO CHECKOUT'))
+});
